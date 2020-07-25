42bd11842526cd5f7764e223cbf38f53aece40b8
Vinyou Tamprateep <vtamprateep@gmail.com>
2020-07-25 17:33:49 -0500

    abstracting data prep

--- main.py
+++ main.py
@@ -7,8 +7,7 @@ import shutil
 def check_args(args):
     assert args.unroll_data and not (args.test_path or args.train_path), "Provide --unroll-data only or --train-path and --test-path."
 
-def check_file(filepath):
-    label_class = ('forest', 'buildings', 'glacier', 'street', 'mountain', 'sea')
+def check_file(filepath, label_class):
     label = filepath.split('/')[-1]
 
     if label in label_class:
@@ -16,8 +15,11 @@ def check_file(filepath):
     else:
         return False
 
-def write_files(rootpath, writepath, filename, label, files):
-    with open(os.path.join(writepath, filename), 'w') as csvfile:
+# Move files to indicated test/train/data file
+def write_files(rootpath, writepath, type_data, label, files):
+    csv_path = os.path.join(writepath, type_data, type_data + '_labels')
+
+    with open(csv_path, 'w') as csvfile:
         csv_writer = csv.DictWriter(csvfile, fieldnames = ['image_id', 'label'])
         csv_writer.writeheader()
 
@@ -29,57 +31,30 @@ def write_files(rootpath, writepath, filename, label, files):
             csv_writer.writerow(csv_entry)
 
             cur_dir = os.path.join(rootpath, image)
-            new_dir = os.path.join(writepath, 'images', image)
+            new_dir = os.path.join(writepath, type_data, 'images', image)
             shutil.move(cur_dir, new_dir)
 
-def prep_pred(path = './data/111880_269359_bundle_archive/seg_pred/seg_pred'):
-    write_path = './data/pred'
+# Identify image classes and create label file
+def prep_data(data_path, write_path, type_data):
+    label_class = os.listdir(data_path)
+    for root, _, files in os.walk(data_path):
+        label = check_file(root, label_class)
+        write_files(root, write_path, type_data, label, files)
 
-    for root, _, files in os.walk(path):
-            label = check_file(root)
-            if not label:
-                continue
-
-            for image in files:
-                cur_dir = os.path.join(root, image)
-                new_dir = os.path.join(write_path, 'image', image)
-                shutil.move(cur_dir, new_dir)
-
-def prep_test(path = './data/111880_269359_bundle_archive/seg_test/seg_test'):
-    write_path = './data/test'
-
-    for root, _, files in os.walk(path):
-        label = check_file(root)
-        if not label:
-            continue
-
-        write_files(root, write_path, 'test_labels', label, files)
-
-def prep_train(path = './data/111880_269359_bundle_archive/seg_train/seg_train'):
-    write_path = './data/train'
-
-    for root, _, files in os.walk(path):
-        label = check_file(root)
-        if not label:
-            continue
-
-        write_files(root, write_path, 'train_labels', label, files)
-
-# Create train, test, pred folders
+# Create train, test folders
 def create_folder(path, split = False):
     if split:
         check_folder = ('train', 'test')
         for i in check_folder:
             folder_path = os.path.join(path, i)
             if not os.path.isdir(folder_path):
-                os.mkdir(folder_path)
-                os.mkdir(folder_path + '/images')
+                os.makedirs(folder_path)
+                os.makedirs(folder_path + '/images')
     else:
         folder_path = os.path.join(path, 'data')
         if not os.path.isdir(folder_path):       
-            os.mkdir(folder_path)
-            os.mkdir(folder_path + '/images')
-
+            os.makedirs(folder_path)
+            os.makedirs(folder_path + '/images')
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -92,13 +67,23 @@ if __name__ == '__main__':
 
     # Validate and assign argument inputs
     args = parser.parse_args()
-    check_args(args)
+    # check_args(args)
 
     write_path = args.write_path
     unroll_data = args.unroll_data
     train_path = args.train_path
     test_path = args.test_path
-    
 
-    create_folder()
-    #prep_test()
+    # Helper vars
+    split_bool = not bool(unroll_data)
+
+    create_folder(write_path, split_bool)
+
+    if test_path:
+        prep_data(test_path, write_path,'test')
+
+    if train_path:
+        prep_data(train_path, write_path, 'train')
+
+    if unroll_data:
+        prep_data(unroll_data, write_path, 'data')
