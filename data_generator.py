d76a74baa61fa2f5bce1e11065cb169de709ef33
Vinyou Tamprateep <vtamprateep@gmail.com>
2020-07-30 20:51:57 -0500

    added validation generator

--- data_generator.py
+++ data_generator.py
@@ -12,25 +12,32 @@ class ImageGenerator(keras.utils.Sequence):
         self.dim = dim
         self.n_classes = n_classes
         self.path = path
+        self.indexes = numpy.arange(len(self.image_label))
+
+        # Shuffle dataset
+        self.on_epoch_end()
 
     def __len__(self):
         return int(numpy.floor(len(self.image_id) / self.batch_size))
 
     def __getitem__(self, index):
-
-        batch_image = self.image_id[index * self.batch_size:(index + 1) * self.batch_size]
-        batch_label = self.image_label[index * self.batch_size:(index + 1) * self.batch_size]
-
-        X, y = self.__data_generation(batch_image, batch_label)
+        batch_index = self.indexes[index * self.batch_size:(index + 1) * self.batch_size]
+        X, y = self.__data_generation(batch_index)
         return X, y
 
+    def on_epoch_end(self):
+        numpy.random.shuffle(self.indexes)
+
     # Generate next mini-batch of data
-    def __data_generation(self, batch_image, batch_label):
-        X = numpy.empty((self.batch_size, *self.dim))
-        for i, name in enumerate(batch_image):
-            image_path = os.path.join(self.path, 'images', name)
+    def __data_generation(self, batch_index):
+        X = numpy.empty((len(batch_index), *self.dim))
+        y = numpy.empty(len(batch_index))
+        for enum, index in enumerate(batch_index):
+            image_path = os.path.join(self.path, 'images', self.image_id[index])
             image_arr = keras.preprocessing.image.load_img(image_path, color_mode = 'rgb', target_size = self.dim)
             image_arr = keras.preprocessing.image.img_to_array(image_arr)
-            X[i,] = image_arr
+            X[enum,] = image_arr
+            y[enum,] = self.image_label[index]
 
-        return X, keras.utils.to_categorical(batch_label, num_classes = self.n_classes)
+        X = X / 255 # Normalize values
+        return X, keras.utils.to_categorical(y, num_classes = self.n_classes)
